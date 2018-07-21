d761bd365b6339d4e438b64777c08b3f0522ef9d
Alyasko <yasko.alexander@gmail.com>
2018-07-21 22:28:07 +0300

    Implemented GetString extension methods, added tests.

---
+++ NetDevTools/NetDevTools.Tests/ExtensionMethods/ByteArrayExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using NetDevTools.ExtensionMethods;
+using Xunit;
+
+namespace NetDevTools.Tests.ExtensionMethods
+{
+    public class ByteArrayExtensionsTests
+    {
+        [Fact]
+        public void GetString_NormalDefaultEncodingBytes_CorrectString()
+        {
+            // Arrange.
+            var expString = "hello";
+            var bytes = Encoding.Default.GetBytes(expString);
+
+            // Act.
+            var actString = bytes.GetString();
+
+            // Assert.
+            Assert.Equal(expString, actString);
+        }
+
+        [Fact]
+        public void GetString_NormalUnicodeEncodingBytes_CorrectString()
+        {
+            // Arrange.
+            var expString = "hello";
+            var bytes = Encoding.Unicode.GetBytes(expString);
+
+            // Act.
+            var actString = bytes.GetStringUnicode();
+
+            // Assert.
+            Assert.Equal(expString, actString);
+        }
+    }
+}
---
+++ NetDevTools/NetDevTools/ExtensionMethods/ByteArrayExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetDevTools.ExtensionMethods
+{
+    public static class ByteArrayExtensions
+    {
+        /// <summary>
+        /// Gets string from bytes buffer using default encoding of OS.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public static string GetString(this byte[] buffer)
+        {
+            return Encoding.Default.GetString(buffer);
+        }
+
+        /// <summary>
+        /// Gets string from bytes buffer using Encoding.Unicode encoding.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public static string GetStringUnicode(this byte[] buffer)
+        {
+            return Encoding.Unicode.GetString(buffer);
+        }
+    }
+}
