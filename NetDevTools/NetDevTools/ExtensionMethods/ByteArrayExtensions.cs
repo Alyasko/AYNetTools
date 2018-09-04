34eeb3eb2e9e4a39534ab3b9baa475b55061802d
Alyasko <yasko.alexander@gmail.com>
2018-09-04 06:19:28 +0300

    Implemented PrefixArraySize.

--- NetDevTools/NetDevTools/ExtensionMethods/ByteArrayExtensions.cs
+++ NetDevTools/NetDevTools/ExtensionMethods/ByteArrayExtensions.cs
@@ -37,5 +37,23 @@ namespace NetDevTools.ExtensionMethods
             if (count < 0) count = buffer.Length;
             return Encoding.Default.GetString(buffer, 0, count);
         }
+
+        /// <summary>
+        /// Creates new array which size equals to current size plus size of array length.
+        /// Inserts the UInt32 length of array into the first bytes of array.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public static byte[] PrefixArraySize(this byte[] buffer)
+        {
+            var dataLength = (UInt32)buffer.Length;
+            var newBuffer = new byte[sizeof(UInt32) + dataLength];
+            var dataLengthBytes = BitConverter.GetBytes(dataLength);
+
+            Buffer.BlockCopy(dataLengthBytes, 0, buffer, 0, sizeof(UInt32));
+            Buffer.BlockCopy(buffer, 0, newBuffer, sizeof(UInt32), buffer.Length);
+
+            return newBuffer;
+        }
     }
 }
